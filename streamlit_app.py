cea8afad0b58a15af4a3b27c6b9fb9de602dbbc1
janie235 <147489820+janie235@users.noreply.github.com>
2023-10-12 21:36:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,7 +96,7 @@ if streamlit.button('Get fruit list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     #closing the snoflake connection
-    my_cnx.close
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
     streamlit.stop()
 
